30cdba88f59cb85e685f9fc90b00d2174eea649c
ray. lee <rlee00@stuy.edu>
2020-03-09 14:43:33 -0400

    Format code

--- lab1.py
+++ lab1.py
@@ -1,18 +1,18 @@
 import plotly.graph_objects as go
-
 import pandas as pd
 
 z_data = pd.read_csv('equipotentialvalues.csv')
 
-fig = go.Figure(data = [go.Surface(
-                                    z=z_data.values
-                                    )]
-                )
+fig = go.Figure(data=[go.Surface(
+    z=z_data.values
+)]
+)
+
 fig.update_traces(contours_z=dict(show=True, usecolormap=True,
                                   highlightcolor="limegreen", project_z=True))
 fig.update_layout(title='Equipotentials', autosize=False,
                   scene_camera_eye=dict(x=1.87, y=0.88, z=-0.64),
                   width=500, height=500,
                   margin=dict(l=65, r=50, b=65, t=90)
-)
+                  )
 fig.show()
